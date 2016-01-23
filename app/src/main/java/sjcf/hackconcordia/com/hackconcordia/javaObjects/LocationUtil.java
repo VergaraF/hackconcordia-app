e96ce4738f68ff2042041a207c29a29091a552dd
Johnny Mak <johnny_mak@outlook.com>
2016-01-23 16:47:46 -0500

    Added a LocationUtil Class

---
+++ app/src/main/java/sjcf/hackconcordia/com/hackconcordia/javaObjects/LocationUtil.java
@@ -0,0 +1,23 @@
+package sjcf.hackconcordia.com.hackconcordia.javaObjects;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by MLH-Admin on 1/23/2016.
+ */
+public class LocationUtil {
+
+    public String localityFinder(Context context, double lat, double lng) {
+
+        Geocoder gcd = new Geocoder(context, Locale.getDefault());
+        List<Address> addresses = gcd.getFromLocation(lat, lng, 0);
+        if (addresses.size() > 0)
+            System.out.println(addresses.get(0).getLocality());
+
+    }
+}
