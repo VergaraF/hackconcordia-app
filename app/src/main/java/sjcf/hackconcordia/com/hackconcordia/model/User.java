465bb0a0d74e0f8f02f2c159f67759169482a53a
tucci <tuccisteven@gmail.com>
2016-01-23 15:10:30 -0800

    Added orm objects

---
+++ app/src/main/java/sjcf/hackconcordia/com/hackconcordia/model/User.java
@@ -0,0 +1,50 @@
+package sjcf.hackconcordia.com.hackconcordia.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.orm.SugarRecord;
+
+public class User extends SugarRecord implements Parcelable {
+
+    String email;
+    String password;// store in plaintext for now. no time
+    int points;
+
+    protected User(Parcel in) {
+        id = in.readLong();
+        email = in.readString();
+        password = in.readString();
+        points = in.readInt();
+    }
+
+    @Override
+    public void save() {
+        super.save();
+        // TODO: connect to backend code to save to db
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(email);
+        dest.writeString(password);
+        dest.writeInt(points);
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[0];
+        }
+    };
+}
